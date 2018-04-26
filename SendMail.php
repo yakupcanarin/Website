5ff4d7d6a26b0a814a4cf821321a29746ab4acb7
Yakup (Jacob) Can Arın <ykpcn.arn@hotmail.com>
2018-04-26 16:25:04 +0300

    Send mail

--- SendMail.php
+++ SendMail.php
@@ -1,27 +1,16 @@
 <?php
+		
+	$Name = $_POST['namesurname']; //senders name 
+	$email = $_POST['mailaddress'];; //senders e-mail adress 
+	$recipient = "c.yakup10@gmail.com"; //recipient 
+	$mail_body = $_POST['mailtext'];; //mail body 
+	$subject = $_POST['subject'];; //subject 
+	$header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields 
+
+	ini_set('smtp','smtp.gmail.com');
+	ini_set('smtp_port','587');
+	ini_set('sendmail_from',$email);
 	
-	use \PHPMailer;
-	date_default_timezone_set('Etc/UTC');
-	
-	require 'C:/wamp64/apps/phpmyadmin4.7.9/vendor/autoload.php';
-	$mail = new PHPMailer;
-	$mail->isSMTP();
-	$mail->SMTPDebug = 2;
-	$mail->Host = 'mail.gmail.com';
-	$mail->Port = 25;
-	$mail->setFrom('c.yakup10@gmail.com', 'Burak Last');
-	$mail->addAddress('ykpcn.arn@hotmail.com', 'Yakup Can Arın');
-	$mail->Subject = 'PHPMailer SMTP without auth test';
-	$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-	$mail->AltBody = 'This is a plain-text message body';
-	if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
-	/*$nameSurname = strip_tags($_POST['namesurname']);
-	$mailaddrs = strip_tags($_POST['mailaddress']);
-	$subject = strip_tags($_POST['subject']);
-	$text = strip_tags($_POST['mailtext']);
-	mail("ykpcn.arn@hotmail.com",$subject,$text);*/
+	mail($recipient, $subject, $mail_body, $header);
+
 ?>
